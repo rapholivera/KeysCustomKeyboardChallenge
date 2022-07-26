6c60e0248a998702ec309a70acb45a68dbe1ac53
Raphael <raphzouz@gmail.com>
2022-07-26 18:30:36 -0300

    improved appearance for dark mode

--- keysCustomKeyboardChallenge/Presentation/Utils/Style/DefaultButtonStyle.swift
+++ keysCustomKeyboardChallenge/Presentation/Utils/Style/DefaultButtonStyle.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct DefaultButtonStyle: ButtonStyle {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
     func makeBody(configuration: Configuration) -> some View {
+        
+        let textColor: Color = (colorScheme == .dark ? .white : .black)
+        let backgroundColor: Color = (colorScheme == .dark ? Color.gray.opacity(0.5) : .white)
         
         configuration
             .label
-            .foregroundColor(configuration.isPressed ? Color.white.opacity(0.5) : .white)
+            .foregroundColor(configuration.isPressed ? textColor.opacity(0.5) : textColor)
             .padding()
-            .background(configuration.isPressed ? Color.accentColor.opacity(0.5) : .accentColor)
+            .background(configuration.isPressed ? backgroundColor.opacity(0.5) : backgroundColor)
             .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .circular))
             .shadow(radius: 2, x: 0, y: 2)
             .scaleEffect(configuration.isPressed ? 0.95 : 1)
